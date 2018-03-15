9399370c2c984c4d0110fb6b8f6520c3a1b26d45
Steve Moyer <smoyer1@selesy.com>
2018-03-15 12:30:59 -0400

    Moves business logic out of startup class.

---
+++ src/main/java/edu/psu/swe/poc/Main.java
@@ -0,0 +1,24 @@
+package edu.psu.swe.poc;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.spi.AfterDeploymentValidation;
+import javax.inject.Inject;
+
+import org.jboss.weld.environment.se.events.ContainerInitialized;
+
+import lombok.extern.slf4j.Slf4j;
+
+@ApplicationScoped
+@Slf4j
+public class Main {
+
+  @Inject
+  WeldSeJarPoc business;
+  
+  public void start(@Observes ContainerInitialized event) {
+    log.trace("start(@Observes ContainerInitialized event)");
+    business.start();
+  }
+
+}
