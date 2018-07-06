fd3bc250e224a15bf8bcec3311159023303bd0e8
Steve Moyer <smoyer@psu.edu>
2018-07-06 12:50:48 -0400

    Updates based on WIP at https://github.com/weld/core/pull/1838

--- src/main/java/edu/psu/swe/poc/DeploymentExceptionHandler.java
+++ src/main/java/edu/psu/swe/poc/DeploymentExceptionHandler.java
@@ -11,14 +11,13 @@ import lombok.extern.slf4j.Slf4j;
 public class DeploymentExceptionHandler implements ExceptionHandler {
 
 	@Override
-	public Stream<Class<? extends Throwable>> getExceptionTypes() {
-		return Stream.of(DeploymentException.class);
-	}
-
-	@Override
 	public void handle(Throwable t) {
         log.error(t.getMessage());
         System.exit(2);
 	}
 
+	@Override
+	public boolean supports(Throwable throwable) {
+		return DeploymentException.class.equals(throwable.getClass());
+	}
 }
